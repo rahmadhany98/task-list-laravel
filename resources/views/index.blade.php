bb28044bc955d7869ecd0ae6d278cd1a2fb739e9
rahmadhany98 <kazuha004@gmail.com>
2024-07-08 05:42:42 +0800

    crud operations

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,16 @@
     {{-- @if (count($tasks)) --}}
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
         </div>
     @empty
         <div>There are no tasks</div>
         {{-- @endif --}}
     @endforelse
+
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
